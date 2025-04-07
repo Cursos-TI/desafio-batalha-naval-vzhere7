95ac86bdf97487daf900cfc537f8fbc085fcbb59
vzhere7 <otaviomerces90@gmail.com>
2025-04-07 23:26:01 +0000

    Adição do Barco 3 e 4 na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,7 +5,59 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
+    int tabuleiro[10][10];
+    // Inicialização do tabuleiro com 0 = água
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+    // Posiciona o navio 1 - horinzontal
+    if (tabuleiro[0][0] == 0 && tabuleiro[0][1] == 0 && tabuleiro[0][2] == 0){
+        tabuleiro[0][0] = 3;
+        tabuleiro[0][1] = 3;
+        tabuleiro[0][2] = 3;
+
+    }
+    // Posiciona o navio 2 - vertical
+    if (tabuleiro[7][9] == 0 && tabuleiro[8][9] == 0 && tabuleiro[9][9] == 0) {
+        tabuleiro[7][9] = 3;
+        tabuleiro[8][9] = 3;
+        tabuleiro[9][9] = 3;
+
+    }
+    //posicionamento do navio 3 = diagonal direita - esquerda - cima - baixo
+    if (tabuleiro[3][3] == 0 && tabuleiro[4][4] == 0 && tabuleiro[5][5] == 0) {
+        tabuleiro[3][3] = 3;
+        tabuleiro[4][4] = 3;
+        tabuleiro[5][5] = 3;
+
+    }
+    // posicionamento do navio 4 = diagonal esquerda - direita - baixo - cima
+    if (tabuleiro[2][6] == 0 && tabuleiro[3][5] == 0 && tabuleiro[4][4] == 0) {
+        tabuleiro[2][6] = 3;
+        tabuleiro[3][5] = 3;
+        tabuleiro[4][4] = 3;
+    } else {
+        if(tabuleiro[6][6] == 0 & tabuleiro[7][5] == 0 && tabuleiro[8][4] == 0){
+            tabuleiro[6][6] = 0;
+            tabuleiro[7][5] = 0;
+            tabuleiro[8][4] = 0;
+
+        }
+    }
+
+    printf("Tabuleiro Batalha Naval: \n");
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d", tabuleiro[i][j]);
+
+        }
+        printf("\n");
+    }
+    return 0;
+
+}    // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
@@ -35,6 +87,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
